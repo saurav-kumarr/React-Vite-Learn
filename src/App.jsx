7b3c231f8e5ea4a65009266da6f0a91fceeec75b
Saurav Kumar <csaurav2014@gmail.com>
2025-10-14 14:07:28 +0530

    Some introduction level learning of React

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,39 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// JSX is a syntax extension for JavaScript that looks like HTML.
 
+// It allows you to write HTML-like code within JavaScript,
+// Which React then transforms into JavaScript objects.
+
+// 1. You can embed any valid JavaScript expression inside curly braces {} in JSX.
+// 2. JSX attributes use camelCase syntax.
+
+function WelcomeMessage(name){
+  return <h1>Hello, {name}!</h1>
+}
+
+function App() {
+  
+    const element = <h1>Hello, world!</h1>
+    const name = "Saurav";
+    const isLoggedin = true;
+    if(isLoggedin){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {WelcomeMessage("Alice")}
+      <h1>Logged in.</h1>
+      {element}
+      {10+2}
+      <h1 className='greeting'>My name is {name}</h1>
+      <button style={{backgroundColor: 'green', color: 'white'}}>Click Me</button>
+      <button onClick={() => alert(`Button Clicked!`)}>
+        Alert Me!
+      </button>
+    </div>
   )
+}
+return <h1>Not logged in.</h1>
 }
 
 export default App
