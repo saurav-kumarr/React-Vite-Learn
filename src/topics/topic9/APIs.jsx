45d2b0b3e5e26316f5b1f5c4b3fae000fb089bcd
Saurav Kumar <csaurav2014@gmail.com>
2025-11-02 00:41:10 +0530

    Making APIs Call with Axios

--- src/topics/topic9/APIs.jsx
+++ src/topics/topic9/APIs.jsx
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 
 function APIs() {
 
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
+        setLoading(true);
         fetch('https://jsonplaceholder.typicode.com/posts/')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json => {
+        setData(json);
+        setLoading(false);
+        //throw new Error('Something went Wrong')
+    })
+
+     axios.get('https://jsonplaceholder.typicode.com/posts/')
+      .then(response => {
+        console.log(response.data)
+        setData(response.data);
+        setLoading(false);
+        //throw new Error('Something went Wrong')
+    })
+
+    .catch((error) => {
+        console.log(`Error fetching data: `, error);
+        setError('Failed to fetch the data');
+        setLoading(false);
+    });
     }, []);
+
+    if(loading) {
+        return <p>Loading...</p>
+    }
+
+    if(error) {
+        return <p>{error}</p>
+    }
 
     return (
         <div>
